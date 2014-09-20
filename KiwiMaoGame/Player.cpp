ae8fd42c0f03ffa859549e29513d7e95e02625e3
1wildman1 <1wildman1@gmail.com>
2014-09-20 13:58:18 -0400

    Player.h and cpp
    I've updated the player.h to hold the protoypes and player.cpp to hold
    the functions.  Let me know how it looks or what should be changed.

--- KiwiMaoGame/Player.cpp
+++ KiwiMaoGame/Player.cpp
@@ -1,52 +1,35 @@
 #include "player.h"
+#include "Card.h"
 #include <iostream>
-using std::cout;
 
 // Precondition: Player must exist.
 // Postcondition: Returns the size vector of the hand. Returns -1 if hand is undefined.
-int Player::getHandSize()
-{
-	return hand.size();
-}
 
-/* DEPRICATED */
-// void Player::setID(int i)
-// {
-// 	ID = i;
-// 	return;
-// }
-
-/* DEPRICATED */
-// int Player::getID()
-// {
-// 	return ID;
-// }
-
-/* DEPRICATED */
-// void Player::setName(string n)
-// {
-// 	name = n;
-// 	return;
-// }
+int Player::getHandSize() {
+		if(hand.empty()){
+			Player::HandSize = -1;
+		} else {
+			Player::HandSize = hand.size();
+		}
+		return HandSize;
+	}
 
 // Precondition: Must access the name variable of the player at the current index.
 // Postcondition: Returns a string of the player's name.
-string Player::getName()
-{
+
+string Player::getName(vector<int> player, int playerNum){
+	Player::name = player[playerNum-1];  //I assume that playerNum will be 2, 3, or 4 for easy cout in the game
 	return name;
 }
 
 // Precondition:
 // Postcondition: Returns the card played
-Card Player::playCard()
-{
-	Card ballon;
-	return ballon;
+Card Player::playCard(int cardIndex){
+	return hand[cardIndex]; //(A variable needs to be passed to this function so that it gives JUST the last card and not the whole hand)
 }
 
 // Precondition:
 // Postcondition: A card from the deck is added to the player's hand. 
-void Player::drawCard()
-{
-	return;
+void Player::drawCard(Card addCard){
+	Player::hand.push_back(addCard);
 }
--- KiwiMaoGame/Player.h
+++ KiwiMaoGame/Player.h
@@ -19,31 +19,20 @@ class Player {
 public:
 	// Precondition: Player must exist.
 	// Postcondition: Returns the size vector of the hand. Returns -1 if hand is undefined.
-	int getHandSize() {
-		if(hand.empty()){
-			Player::HandSize = -1;
-		} else {
-			Player::HandSize = hand.size();
-		}
-		return HandSize;
-	}
+	int getHandSize();
 
 	// Precondition: Must access the name variable of the player at the current index.
 	// Postcondition: Returns a string of the player's name.
-	string getName(vector<int> player, int playerNum){
-		Player::name = player[playerNum-1];  //I assume that playerNum will be 2, 3, or 4 for easy cout in the game
-		return name;
-	}
+	string getName(vector<int> player, int playerNum);
+
 	// Precondition:
 	// Postcondition: Returns the card played
-	Card playCard(int cardIndex){
-		return hand[cardIndex]; //(A variable needs to be passed to this function so that it gives JUST the last card and not the whole hand)
-	}
+	Card playCard(int cardIndex);
+
 	// Precondition:
 	// Postcondition: A card from the deck is added to the player's hand. 
-	void drawCard(Card addCard){
-		Player::hand.push_back(addCard);
-	}
+	void drawCard(Card addCard);
+
 private:
 	int ID, HandSize;
 	vector<Card> hand;
