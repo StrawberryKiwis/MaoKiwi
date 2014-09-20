fbbc573bf884be02d4ea4acda2b6515e407d5457
1wildman1 <1wildman1@gmail.com>
2014-09-20 13:31:15 -0400

    Player.h
    So.... I added a bunch of stuff to player.  I think the biggest
    difficulty is that I'm not entirely sure if I'm asking for the correct
    input from GameMao.  I'd like to ask for the player vector that holds
    0-3 and an actual number that represents which player 1-4 to be handed
    to getName.  Check it out and let me know if there is anything "off".

--- KiwiMaoGame/Player.h
+++ KiwiMaoGame/Player.h
@@ -1,6 +1,12 @@
 #ifndef ___PLAYER_H
 #define ___PLAYER_H
 
+// File:  Player.h
+// Date:  9/19/2014
+// Class:  COMP 220, Computer Programming II
+// Contents:  This player header will hold all the info of the player.
+  
+
 #include <string>
 using std::string;
 
@@ -9,15 +15,37 @@ using std::vector;
 
 #include "card.h"
 
-class Player
-{
+class Player {
 public:
-	int getHandSize();
-	string getName();
-	Card playCard();
-	void drawCard();
+	// Precondition: Player must exist.
+	// Postcondition: Returns the size vector of the hand. Returns -1 if hand is undefined.
+	int getHandSize() {
+		if(hand.empty()){
+			Player::HandSize = -1;
+		} else {
+			Player::HandSize = hand.size();
+		}
+		return HandSize;
+	}
+
+	// Precondition: Must access the name variable of the player at the current index.
+	// Postcondition: Returns a string of the player's name.
+	string getName(vector<int> player, int playerNum){
+		Player::name = player[playerNum-1];  //I assume that playerNum will be 2, 3, or 4 for easy cout in the game
+		return name;
+	}
+	// Precondition:
+	// Postcondition: Returns the card played
+	Card playCard(int cardIndex){
+		return hand[cardIndex]; //(A variable needs to be passed to this function so that it gives JUST the last card and not the whole hand)
+	}
+	// Precondition:
+	// Postcondition: A card from the deck is added to the player's hand. 
+	void drawCard(Card addCard){
+		Player::hand.push_back(addCard);
+	}
 private:
-	int ID;
+	int ID, HandSize;
 	vector<Card> hand;
 	string name;
 };
