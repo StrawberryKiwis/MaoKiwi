f91e04051757dc3129153d59a4afbda9fb103032
ComNerd <luigimtj@msn.com>
2014-09-21 22:49:20 -0400

    Added some functions and initials
    As I was testing out the code, I figured that it would be a lot easier if I added the following functions:
    deal, addPlayer, getReverse, findCardIndex, changeTurn, cardsInDeck, noCardsInHand.
    
    Nobody will likely have to deal with these as I only use them in the functions and code I work with.
    
    Also added some initialization that was needed for some functions to work properly.

--- KiwiMaoGame/gameMao.h
+++ KiwiMaoGame/gameMao.h
@@ -4,24 +4,55 @@
 #include <vector>
 using std::vector;
 
-#include "card.cpp"
-#include "player.cpp"
+//Added random and time.h for generating random numbers
+#include <random>
+using std::srand;
+
+#include <time.h>
+
+#include "Player.h"
+#include "Card.h"
 
 class GameMao
 {
 public:
+	//Added some needed initialization.
+	GameMao(int playerTurn = 0, bool reverse = false, int drawTwoCount = 0) : 
+		playerTurn(0), reverse(false), drawTwoCount(0) {}
 	bool checkLegalMove(Card x);
 	void shuffle();
-	void lastCard();
-	void playerCardCount();
+	
+	//Added deal() to start the game.
+	//Should shuffle all the cards in the deck, give seven to each player
+	//and then place one card on the discard pile.
+	void deal();
+	Card lastCard();
 	bool winCondition(Player y);
 	void cardFunction(Card x);
-	void playerPlay();
+	void playerPlay(Player& y);
+	vector<Player>& getPlayerVec();
+	
+	//Adds a player to vector<player> players
+	void addPlayer(Player y);
+	bool getReverse();
+	
+	//Locates the card chosen by a player
+	int findCardIndex(Player& y, Card x);
+	int getPlayerTurn();
+	
+	//Changes whos turn it is
+	void changeTurn();
+	
+	//Prints out how many cards are still in the deck
+	void cardsInDeck();
+	void noCardsInHand(Player& y);
 private:
 	int drawTwoCount;
 	int playerTurn;
 	vector<Card> discard;
 	vector<Card> deck;
+	vector<Player> players;
+	bool reverse;
 };
 
 #endif
