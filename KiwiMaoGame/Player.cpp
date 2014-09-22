6d8a1e6e8208658e92f3219f1587d589e53eab02
1wildman1 <1wildman1@gmail.com>
2014-09-22 16:52:44 -0400

    Updated (close to fin) Player cpp/header
    I modified my code according to Matt's comments and it should work
    fluidly with the game.

--- KiwiMaoGame/Player.cpp
+++ KiwiMaoGame/Player.cpp
@@ -1,30 +1,47 @@
 #include "player.h"
 #include "Card.h"
 #include <iostream>
+using namespace std;
 
 // Precondition: Player must exist.
-// Postcondition: Returns the size vector of the hand. Returns -1 if hand is undefined.
+// Postcondition: Returns the size vector of the hand. 
 
 int Player::getHandSize() {
 		if(hand.empty()){
-			Player::HandSize = -1;
+			HandSize = 0;
 		} else {
-			Player::HandSize = hand.size();
+			HandSize = hand.size();
 		}
+		cout<< "Your handsize is " << HandSize;
 		return HandSize;
 	}
 
 // Precondition: Must access the name variable of the player at the current index.
 // Postcondition: Returns a string of the player's name.
-
-string Player::getName(vector<int> player, int playerNum){
-	Player::name = player[playerNum-1];  //I assume that playerNum will be 2, 3, or 4 for easy cout in the game
+string Player::getName(){
 	return name;
 }
 
 // Precondition:
 // Postcondition: Returns the card played
-Card Player::playCard(int cardIndex){
+Card Player::playCard(){
+	cout << "Your hand includes " << endl;
+	cardIndex = hand.size();
+
+	for(int i = 0; i < cardIndex; i++){
+		cout << i + 1 << " " << hand[i].name() << endl;
+	}
+	bool check = false;
+	while(check == false){
+	cin >> cardIndex;
+	cardIndex--;
+	if(cardIndex > hand.size()){
+		check = false;
+		cout << "Please put in a correct value between 1 and " << hand.size()-1 ;
+	} else {
+		check = true;
+	}
+}
 	return hand[cardIndex]; //(A variable needs to be passed to this function so that it gives JUST the last card and not the whole hand)
 }
 
